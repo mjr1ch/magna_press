93063e04ea5a7d181a3d66d50a9a7cd9a4e72eea
Demo User <debian@beaglebone.localdomain>
2020-07-24 03:15:57 +0000

    new push

--- decoder.pasm
+++ decoder.pasm
@@ -1,42 +1,42 @@
-include "common.h"
-  
-#define counter r10
-#define pin_A   r31.t2
-#define pin_B   r31.t6
-  
-.macro increment
-     add     counter, counter, 1
-     sbco    &counter, c24, 0, 4     // store counter to memory
-.endm
+#include "common.h"
  
-.macro decrement
+ #define counter r10
+ #define pin_A   r31.t2
+ #define pin_B   r31.t6
+ 
+ .macro increment
+     add     counter, counter, 1
+     sbco    &counter, c28, 0, 4     // store counter to memory
+ .endm
+ 
+ .macro decrement
      sub     counter, counter, 1
-     sbco    &counter, c24, 0, 4     // store counter to memory
-.endm
+     sbco    &counter, c28, 0, 4     // store counter to memory
+ .endm
  
  
      mov     counter, 0
-     sbco    &counter, c24, 0, 4     // store initial counter to memory
+     sbco    &counter, c28, 0, 4     // store initial counter to memory
  
-// jump into right place of the loop based on initial state of A
+ // jump into right place of the loop based on initial state of A
      bbs     Ahigh,    pin_A
      jmp     Alow
  
-A0_Bfall:
+ A0_Bfall:
      increment                       // B falling edge with A low
  
-Alow:
+ Alow:
      wbs     pin_A                   // A is low, wait for A rise
      bbs     Arise_B1, pin_B         // A rising edge, check B
  
      increment                       // A rising edge with B low
      jmp     Blow
-
-
-Afall_B0:
+ 
+ 
+ Afall_B0:
      decrement                       // A falling edge with B low
  
-Blow:
+ Blow:
      wbs     pin_B                   // B is low, wait for B rise
      bbs     A1_Brise, pin_A         // B rising edge, check A
  
@@ -44,20 +44,20 @@ Blow:
      decrement                       // B rising edge with A low
      jmp     Alow
  
-A1_Brise:
+ A1_Brise:
      increment                       // B rising edge with A high
  
-Ahigh:
+ Ahigh:
      wbc     pin_A                   // A is high, wait for A fall
      bbc     Afall_B0, pin_B         // A falling edge, check B
-
+ 
      increment                       // A falling edge with B high             
      jmp     Bhigh 
                                                               
-Arise_B1: 
+ Arise_B1: 
      decrement                       // A rising edge wit 
       
-Bhigh:   
+ Bhigh:   
      wbc     pin_B                   // B is high, wait for B fall 
      bbc     A0_Bfall, pin_A         // B falling edge, check A 
                                                                       
--- demo_main.py
+++ demo_main.py
@@ -1,9 +1,12 @@
 from uio.ti.icss import Icss
+from ctypes import c_uint32 as uint32, c_uint8 as uint8
 from PyQt5 import QtWidgets
 from PyQt5.QtCore import pyqtSlot
 import sys # We need sys so that we can pass argv to QApplication
+import cursor
 from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QMessageBox
-
+from number_pad import numberPopup
+#from numpad_popup import numpadPopup
 from demo_window import Ui_MainWindow # This file holds our MainWindow and all design related things
               # it also keeps events etc that we defined in Qt Designer
 
@@ -11,14 +14,24 @@ from demo_window import Ui_MainWindow # This file holds our MainWindow and all d
 class MainWindow(QtWidgets.QMainWindow):
     
     pruss = Icss( "/dev/uio/pruss/module" )
+    ui = Ui_MainWindow()
+    core_0 = pruss.core0
+    core_1 = pruss.core1
+    position = None 
+    weight = None 
+
     
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent=parent)
-        ui = Ui_MainWindow()
-        ui.setupUi(self)
-        ui.bttn_close.clicked.connect(self.CloseButton_on_click)
-        ui.bttn_captureZero.clicked.connect(self.CaptureZeroButton_on_click)
+        self.ui.setupUi(self)
+        self.ui.bttn_close.clicked.connect(self.CloseButton_on_click)
+        self.ui.bttn_captureZero.clicked.connect(self.CaptureZeroButton_on_click)
+        self.ui.bttn_setSpan.clicked.connect(self.SetSpanButton_on_click)
+        self.ui.bttn_tareScale.clicked.connect(self.TareScaleButton_on_click)
         self.pruss.uart.initialize(baudrate = 9600)
+        self.core_0.load('decoder.bin')
+        self,core_0.run()
+        self.poistion = self.pruss.dram0.map( uint32 )
         self._init_EM100()
 
     def _trimMessage(self,message,start,finish):
@@ -76,28 +89,38 @@ class MainWindow(QtWidgets.QMainWindow):
    
     @pyqtSlot()
     def SetSpanButton_on_click(self):
+        self.setEnabled(False)
+        self.exPopup = numberPopup()
+        self.exPopup.setGeometry(130, 320,400, 300)
+        self.exPopup.show()  
         self.pruss.uart.io.write(b'CG\r')
         self.pruss.uart.io.readline(newline =b'\r')
         self._openCalibration()
-        self.pruss.uart.io.write(bytes('CG ' + self.tb_span.value + '\r','ascii'))
-        self.pruss.uart.io.readline(newline =b'\r')
+        self.pruss.uart.io.write(bytes('CG ' + self.ui.tb_span.text() + '\r','ascii'))
+        response = self.pruss.uart.io.readline(newline =b'\r').decode('ascii')
+        QMessageBox.about(self, "Response", response)
+
 
     @pyqtSlot()
-    def TareButton_on_click(self):
+    def TareScaleButton_on_click(self):
         self.pruss.uart.io.write(b'ST\r')
-        self.pruss.uart.io.readline(newline =b'\r')
+        response = self.pruss.uart.io.readline(newline =b'\r').decode('ascii')
+        QMessageBox.about(self, "Response", response)
 
     @pyqtSlot()
     def CaptureZeroButton_on_click(self):
         self._openCalibration()
-        pass
+        self.pruss.uart.io.write(b'CZ\r')
+        response = self.pruss.uart.io.readline(newline =b'\r').decode('ascii')
+        QMessageBox.about(self, "Response", response)
 
     @pyqtSlot()
     def PollSensorButton_on_click(self):
         self.pruss.uart.io.write(b'CG\r')
         response = self.pruss.uart.io.readline(newline =b'\r')
         weight = self._trimMessage(response,2,len(response))
-        self.lcd_weight.value = str(weight)
+        self.lcd_weight.display = str(weight)
+        self.lcd_position.display = self.position
 
     @pyqtSlot() 
     def StartButton_on_click(self):
@@ -116,6 +139,7 @@ def main():
     screen = MainWindow()
     screen.show()
     sys.exit(app.exec())
+    core_0.halt()
 
 if __name__ == '__main__':
     main()
